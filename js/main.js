ec0f4b056171d68e9457f4f58ee5b1f42e203613
Valeria Oshiro <valeriaoshiro19@gmail.com>
2017-09-11 16:38:24 -0700

    Implemented hover on cards to display links

--- js/main.js
+++ js/main.js
@@ -2,14 +2,12 @@ $(document).ready(function(){
 	//when the user clicks on a project picture, it shows the links for the code and site
 	$('.card').on('click', function(){
 		if($(this).children('.card-front').hasClass('hidden')){
-			console.log('.card-front is hidden');
 			$(this).children('.card-back').slideUp(200);
 			$(this).children('.card-back').addClass('hidden');
 			// $('.card-front').show(150);
 			$(this).children('.card-front').removeClass('hidden');
 
 		} else {
-			console.log('.card-back is hidden');
 			// $('.card-front').hide(500);
 			$(this).children('.card-front').addClass('hidden');
 			$(this).children('.card-back').slideDown(200);
@@ -17,25 +15,26 @@ $(document).ready(function(){
 		}
 		
 	});
+	//when the user mousenter
+	$('.card').hover(
+		function(){
+			// $('.card-front').hide(500);
+			$(this).children('.card-front').addClass('hidden');
+			$(this).children('.card-back').slideDown(200);
+			$(this).children('.card-back').removeClass('hidden');
+		},
+		function(){
+			$(this).children('.card-back').slideUp(200);
+			$(this).children('.card-back').addClass('hidden');
+			// $('.card-front').show(150);
+			$(this).children('.card-front').removeClass('hidden');
+		}
+	);
 
 	//when using a small screen, the navbar changes to a hamburger
 	//when the hamburger is clicked, it opens the menu
 	$('.burger-nav').on('click', function(){
 		$('.navbar ul').toggleClass('open');
-	});
-
-	// Send form to my email
-	var messageForm = "";
-	$(".send").on("click", function(){
-	  messageForm = $("form").serialize();
-	  $.ajax({
-	    url: "https://formspree.io/contact@valeriaoshiro.com", 
-	    method: "POST",
-	    data: { message: messageForm },
-	    dataType: "json"
-	  });
-	  alert("Thank you for the email. We'll be in touch promptly.");
-	  return false;
 	});
 
 	//up button appears when scrolled a certain point 
